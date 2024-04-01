3152db751d29e76ace5ddebb13c3ba0bd0d1caf9
yongZhang <kinmer7@gmail.com>
2024-04-01 16:26:03 -0400

    modify bookSchema

--- mongoose-books/models/book.js
+++ mongoose-books/models/book.js
@@ -5,14 +5,25 @@ const bookSchema = new Schema({
         title: { type: String, required: true },
         author: { type: String, required: true },
         publisher: { type: String, required: true },
-        releaseDate: { type: Date, required: true },
-        ASIN: { type: Date, required: true },
+        publishYear: { type: Number, required: true },
+        ISBN: { type: String, required: true },
         customerRating: { 
-            type: Date,
-            enum:['1', '2', '3', '4', '5'] 
+            type: Number,
+            enum:[1, 2, 3, 4, 5] 
          },
     } , {
         timestamps: true
     });
 
 module.exports = mongoose.model('Book', bookSchema);
+
+
+// const Book = require('./models/book')
+
+// Book.create({
+//     title: 'The Fire Next Time',
+//         author: 'James Baldwin',
+//         publisher: 'Modern Library',
+//         publishYear: 2021,
+//         ISBN: '978-0679601517',
+//         customerRating: 4}).then(console.log)
