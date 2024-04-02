c3f2a241e4433ec87e6f78e6f9eaa521f1d07c55
yongZhang <kinmer7@gmail.com>
2024-04-02 16:48:13 -0400

    add reviewSchema

--- mongoose-books/models/book.js
+++ mongoose-books/models/book.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    content: {
+      type: String,
+      required: true
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      default: 3
+    }
+  }, {
+    timestamps: true
+  });
+
 const bookSchema = new Schema({
         title: { type: String, required: true },
         author: { type: String, required: true },
@@ -11,10 +26,17 @@ const bookSchema = new Schema({
                         } 
         },
         ISBN: { type: String },
+        reviews: [reviewSchema]
 
     } , {
         timestamps: true
     });
+
+
+
+
+
+    
 
 module.exports = mongoose.model('Book', bookSchema);
 
